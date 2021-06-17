b83a228ba393dfa94f149b58904c1e8dca84694e
Breno Douglas <breninho94@gmail.com>
2021-06-17 11:46:12 -0300

    Add cache for steps

--- prod.config.js
+++ prod.config.js
@@ -3,6 +3,7 @@ module.exports = {
   env: {
     PM2_SERVE_PATH: './dist',
     PM2_SERVE_SPA: 'true',
-    PM2_SERVE_HOMEPAGE: './index.html'
+    PM2_SERVE_HOMEPAGE: './index.html',
+    PM2_SERVE_PORT: process.env.PORT || 8080,
   }
 };
