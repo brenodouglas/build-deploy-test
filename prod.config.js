96f788de96d60021b486953c00326672cc3124a7
Breno Douglas <breninho94@gmail.com>
2021-06-17 11:50:43 -0300

    change for test cache

--- prod.config.js
+++ prod.config.js
@@ -2,7 +2,7 @@ module.exports = {
   script: "serve",
   env: {
     PM2_SERVE_PATH: './dist',
-    PM2_SERVE_SPA: 'true',
+    PM2_SERVE_SPA: true,
     PM2_SERVE_HOMEPAGE: './index.html',
     PM2_SERVE_PORT: process.env.PORT || 8080,
   }
