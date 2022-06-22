334c6eb394685a6d2be7068615d96a096d37df78
Breno Douglas <breninho94@gmail.com>
2022-06-22 12:44:59 -0300

    feat: add another comment

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const port = process.env.PORT || 8080
 //here we are configuring dist to serve app files
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
+// add another comment
 // this * route is to serve project on different page routes except root `/`
 app.get(/.*/, function (req, res) {
 	res.sendFile(path.join(__dirname, '/dist/index.html'))
