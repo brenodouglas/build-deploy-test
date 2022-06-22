d294954199bba00abed5439ac40638e54abb8d78
Breno Douglas <breninho94@gmail.com>
2022-06-22 12:43:14 -0300

    refactor: remove comment

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 8080
 //here we are configuring dist to serve app files
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
-// add comment for commit - this * route is to serve project on different page routes except root `/`
+// this * route is to serve project on different page routes except root `/`
 app.get(/.*/, function (req, res) {
 	res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
